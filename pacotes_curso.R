93d64dc375dd3b78bd5d22ca9e2ae0be1fbf4c6f
Henrique Gomide <henriquepgomide@gmail.com>
2020-11-09 10:43:24 -0300

    add packages for course

--- pacotes_curso.R
+++ pacotes_curso.R
@@ -1,1 +1,24 @@
 # Pacotes para curso R
+# Lembre-se. A Versão esperada do seu R é a 4.x.
+
+# Processamento de dados
+library(tidyverse)# Coleção de pacotes para manipulação dos dados 
+library(scales)   # Melhora as escalas dos gráficos
+library(plotly)   # Gráficos interativos
+library(slider)   # Cria médias móveis
+ 
+# Mapas
+library(rgdal)    # Processa GeoJson
+library(leaflet)  # Mapas interativos em Javascript
+
+# Shiny 
+library(shiny)    # Estrela principal do curso. Cria dashboards
+library(bs4Dash)  # Tema para criação da dashboard
+library(fresh)    # Edição de temas para customização do dashboard
+
+# Pacotes para análise epidemiológica da COVID-19
+library(distcrete)
+library(epitrix)
+library(incidence)
+library(projections)
+library(EpiEstim)
