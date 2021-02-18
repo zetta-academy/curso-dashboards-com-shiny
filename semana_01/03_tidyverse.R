4887a184fecbb77999056b9956cfd8a3187760d4
Henrique Gomide <henriquepgomide@gmail.com>
2021-02-17 22:03:20 -0300

    add exercises to tidyverse script

--- semana_01/03_tidyverse.R
+++ semana_01/03_tidyverse.R
@@ -1,11 +1,14 @@
 # Zetta Health Academy - Curso Dashboards com Shiny
 # URL: https://github.com/zetta-academy/curso-dashboards-com-shiny
-# Autor: Henrique Gomide, Ph.D., UFV
+# Autor: Gabriel Kakizaki, Zetta, UFV 
 
-# Estatística básica com R (tidyverse)
-library(tidyverse)
+
+# Estatística básica com R (tidyverse) ------------------------------------
+
 # Tidyverse é uma biblioteca para o R que busca facilitar a análise de dados.
 # Vamos ver como utilizar algumas de suas principais funções
+library(tidyverse)
+
 
 # Funcoes - Baixar dados da API do COVID-19 Brasil
 # use_cached_data (logical): tenta usar dados já baixados
@@ -37,6 +40,7 @@ fetch_data_brasil_io <- function(use_cached_data = TRUE) {
   }
 }
 
+
 # O código abaixo está comentado para explicar como funcionam as funções dos
 # pacotes do Tidyverse
 # Processar os dados da API
@@ -56,7 +60,7 @@ pre_process_covid_data <- function(data) {
       arrange(date) %>%
       mutate(selector = paste0(city, " - ", state,
                                " (", city_ibge_code, ")")) %>%
-      # "group_by" deve ser familiar se você já trabalhou com pandas. Ela
+      # "group_by" deve ser familiar se você já trabalhou com pandas ou SQL. Ela
       # funciona agrupando o dataset com base nos valores de uma coluna. Por
       # exemplo, você pode agrupar por cidade e aplicar a soma no número de
       # casos.
@@ -79,19 +83,33 @@ pre_process_covid_data <- function(data) {
 # Abrir banco de dados
 covid19 <- fetch_data_brasil_io(use_cached_data = TRUE)
 
-
-
+# Selecionar a capital SP
 city_name <- "São Paulo - SP"
 
+# Ler arquivo com códigos das cidades
 cities_with_codes <- readLines("data/city_selector.gz", encoding = "utf-8")
+
+
+
+# Exercícios --------------------------------------------------------------
+# 01 - Tente identificar o que as linhas abaixo fazem.
+# Dica: Use a documentação do R (Estes comandos são uteis: ?nome_funcao; e.g., 
+#       ?grep)
 city_string <- cities_with_codes[grep(city_name, cities_with_codes)[[1]]]
 city_code <- as.integer(str_extract(city_string, "\\d+"))
 
+
+# 02 - O que os comandos das linhas  100-103 fazem?
+# Desafio: Melhore a qualidade da imagem. Procure a 
+#          cola (cheatsheet) em Help > Cheatsheets > Data Visualization...
+#          Não se esqueça de postar seu desafio no Discord da Zetta. 
 covid19 %>%
   filter(city_ibge_code == city_code) %>%
   ggplot(aes(date, new_confirmed)) +
     geom_line()
 
+
+# 03 - O que os comandos abaixo fazem?
 covid19 %>%
   group_by(city_ibge_code) %>%
   mutate(cases_per_100k = sum(confirmed_n)) %>%
