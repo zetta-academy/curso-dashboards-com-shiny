e6484f5845ee2089f9f24f7bc45b1d4f13fbffc8
Henrique Gomide <henriquepgomide@gmail.com>
2021-02-17 21:53:51 -0300

    improve exercises

--- semana_01/template_shiny/shiny_desafio.R
+++ semana_01/template_shiny/shiny_desafio.R
@@ -6,27 +6,18 @@ library(shiny)
 
 
 ui <- fluidPage(
-  # Dica: use textInput e textOuput
-  # Cola: https://raw.githubusercontent.com/rstudio/cheatsheets/master/shiny.pdf 
-  # Edite as linhas abaixo
-  title = "Minha aplicação Shiny",
-  h1("Desafio 1"),
-  p("Crie uma aplicação Shiny que receba um texto como input e 
-retorne este texto como output."),
   textInput("nameInput", 
             label = "Escreva seu nome:"),
   textOutput("nameOutput")
 )
 
 
-
 server <- function(input, output, session) {
   # Codigo do servidor
   output$nameOutput <- renderText({
-    # Edite as linhas abaixo #
-    # Seu código aqui:
     input$nameInput
   })
 }
+
 
 shinyApp(ui = ui, server = server)
